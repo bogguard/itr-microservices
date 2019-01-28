3bd827500575de757c1b782bc325fef229913e9b
Tim Bogaert <bogaertt@Tims-MacBook-Pro.local>
2019-01-28 17:03:26 +0100

    adding commit

---
+++ src/main/java/io/itrw/workshops/microservices/demo/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package io.itrw.workshops.microservices.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hi canary";
+    }
+
+}
